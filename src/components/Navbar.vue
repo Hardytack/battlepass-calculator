6bd417c39e405ec52fa76a655cdd4864bd8da4ab
Michael Boro <michaelboro1122@gmail.com>
2020-09-08 15:42:43 -0400

    Init Commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,25 @@
+<script>
+export default {
+  name: "Navbar",
+  props: {
+    title: String,
+  },
+};
+</script>
+
+<template>
+  <div id="nav">
+    <router-link to="/">Home</router-link>|
+    <router-link to="/about">About</router-link>
+  </div>
+  <!-- <router-view /> -->
+</template>
+
+<style lang="scss" scoped>
+$red: red;
+#navbar {
+  h1 {
+    color: $red;
+  }
+}
+</style>
