257495275415e00c8789ef8e4466d547c2e218b2
Michael Boro <michaelboro1122@gmail.com>
2020-09-14 19:11:49 -0400

    updated vscode formatter

--- server/app.py
+++ server/app.py
@@ -3,9 +3,14 @@ from flask_marshmallow import Marshmallow
 from flask_sqlalchemy import SQLAlchemy
 import os
 
+# Initialize App
 app = Flask(__name__)
-
-@app.route('/', methods=["GET"])
+# App Routes
+@app.route("/", methods=["GET"])
 def text_route():
     return jsonify({"name": "python"})
-app.run(debug=True)
+
+
+# Run App
+if __name__ == "__main__":
+    app.run(debug=True)
