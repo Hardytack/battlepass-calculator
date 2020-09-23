25dedfb6d9804445c204af1cf6f562e4cebe5b70
Michael Boro <michaelboro1122@gmail.com>
2020-09-23 15:36:52 -0400

    finished register and login funcs

--- server/BP_Server/auth.py
+++ server/BP_Server/auth.py
@@ -1,4 +1,6 @@
-import functools
+import functools, os, datetime
+
+import jwt
 
 from flask import (
     Blueprint,
@@ -11,6 +13,7 @@ from flask import (
     Response,
     jsonify,
     json,
+    current_app,
 )
 from werkzeug.security import check_password_hash, generate_password_hash
 
@@ -19,8 +22,53 @@ from BP_Server.db import get_db
 bp = Blueprint("auth", __name__, url_prefix="/auth")
 
 
+def generate_token(user_id):
+    try:
+        payload = {
+            "exp": datetime.datetime.utcnow()
+            + datetime.timedelta(days=0, minutes=10, seconds=5),
+            "iat": datetime.datetime.utcnow(),
+            "sub": user_id,
+        }
+        return jwt.encode(
+            payload, current_app.config.get("SECRET_KEY"), algorithm="HS256"
+        )
+    except Exception as e:
+        return e
+
+
+def decode_token(auth_token):
+    try:
+        payload = jwt.decode(auth_token, current_app.config.get("SECRET_KEY"))
+        return payload["sub"]
+    except jwt.ExpiredSignatureError:
+        return "Signature expired. Please log in again"
+    except jwt.InvalidTokenError:
+        return "Invalid token. Please try again"
+
+
+def verify_user(user_request):
+    db = get_db()
+    auth_header = user_request.headers.get("Authorization")
+    if auth_header:
+        auth_token = auth_header.split(" ")[1]
+    else:
+        auth_token = ""
+    if auth_token:
+        decoded_token = decode_token(auth_token)
+        print(decoded_token)
+        if not isinstance(decoded_token, str):
+            user = db.execute(
+                "SELECT * FROM users WHERE id = ?", (decoded_token,)
+            ).fetchone()
+            if user is not None:
+                return True
+    return False
+
+
 @bp.route("/login", methods=["POST"])
 def login():
+    db = get_db()
     content = request.get_json()
     username = content["username"]
     password = content["password"]
@@ -34,7 +82,24 @@ def login():
         error = {"message": "Username and/or Password are incorrect"}
 
     if error is None:
-        session.clear()
+        auth_token = generate_token(user["id"])
+        my_response = Response(
+            response=json.dumps(
+                {
+                    "message": "user logged in!",
+                    "auth_token": auth_token.decode(),
+                    "username": user["username"],
+                }
+            ),
+            status=200,
+            mimetype="application/json",
+        )
+        return my_response
+    else:
+        my_response = Response(
+            response=json.dumps(error), status=500, mimetype="application/json"
+        )
+        return my_response
 
 
 @bp.route("/register", methods=["POST"])
@@ -61,8 +126,18 @@ def register():
             (username, generate_password_hash(password)),
         )
         db.commit()
+        user = db.execute(
+            "SELECT * FROM users WHERE username = ?", (username,)
+        ).fetchone()
+        auth_token = generate_token(user["id"])
         my_response = Response(
-            response=json.dumps({"message": "user created!"}),
+            response=json.dumps(
+                {
+                    "message": "user created!",
+                    "auth_token": auth_token.decode(),
+                    "username": user["username"],
+                }
+            ),
             status=201,
             mimetype="application/json",
         )
@@ -78,11 +153,31 @@ def register():
 @bp.route("/hello", methods=["GET", "POST"])
 def bp_test():
 
-    # Verifying if an input is in a list
-    content = request.get_json()
-    name = content["username"]
-    my_names = ["hardy", "sarah", "astrid"]
-    if name in my_names:
-        return "its here!"
+    # # Verifying if an input is in a list
+    # content = request.get_json()
+    # name = content["username"]
+    # my_names = ["hardy", "sarah", "astrid"]
+    # if name in my_names:
+    #     return "its here!"
+    # else:
+    #     return "not in the list"
+
+    # db = get_db()
+    # my_token = generate_token(3)
+    # print(my_token)
+    # decoded = decode_token(my_token)
+    # print(decoded)
+
+    # user = db.execute("SELECT * FROM users WHERE id = ?", (decoded,)).fetchone()
+
+    # if user is not None:
+    #     print(user["username"])
+    # else:
+    #     print("not found")
+
+    # return "check console"
+    result = verify_user(request)
+    if result:
+        return "you're good!"
     else:
-        return "not in the list"
+        return "nana"
